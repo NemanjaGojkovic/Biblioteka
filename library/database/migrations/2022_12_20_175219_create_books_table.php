f4a9ca8855e8d525f6dbc6857fc25c6696de01b4
NemanjaGojkovic <n.gojko6@gmail.com>
2022-12-20 20:07:38 +0100

    1.  napravljeni svi modeli

---
+++ library/database/migrations/2022_12_20_175219_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('category');
+            $table->timestamps();
+            $table->date('publish_date');
+            $table->foreignId('category_id');
+            $table->foreignId('author_id');
+            $table->foreignId('publisher_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
